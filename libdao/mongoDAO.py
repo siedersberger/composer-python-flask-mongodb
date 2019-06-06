38c80e4dab00648b7edd47f370be5ece084d50db
siedersberger <daniel.srg@hotmail.com>
2019-06-05 21:31:46 -0300

    Conflict fixed

--- libdao/mongoDAO.py
+++ libdao/mongoDAO.py
@@ -1,26 +1,55 @@
 from pymongo import MongoClient
 
+class ConfigManager:
+    __instance = None
+    
+    @staticmethod
+    def instance():
+        if not ConfigManager.__instance:
+            ConfigManager.__instance = {
+                "mongo_port": 27017,
+                "mongo_host": "localhost"
+            }
+        return ConfigManager.__instance
+
 class ConnectionDAO:
+    __instance = None
+    
+    @staticmethod
+    def instance():
+        if not ConnectionDAO.__instance:
+            configs = ConfigManager.instance()
+            ConnectionDAO.__instance = MongoClient(
+                port=configs["mongo_port"],
+                host=configs["mongo_host"]
+            )
+        return ConnectionDAO.__instance
 
-    def __init__(self, hostname="localhost", portnumber=27017):
-        self.__client = MongoClient(host=hostname, port=portnumber)
-        self.__db = self.client.dbstack
+# class ConnectionDAO:
 
-    @property
-    def client(self):
-        return self.__client
+#     def __init__(self, hostname="localhost", portnumber=27017):
+#         self.__client = MongoClient(host=hostname, port=portnumber)
+#         self.__db = self.client.dbstack
 
-    @property
-    def db(self):
-        return self.__db
+#     @property
+#     def client(self):
+#         return self.__client
+
+#     @property
+#     def db(self):
+#         return self.__db
 
 class QuestionDAO:
 
-    def __init__(self, db):
-        self.my_collection = db["questions_collection"]
+    def __init__(self):
+        self.__db_client = ConnectionDAO.instance()
+        self.__questions_collection = self.__db_client.dbstack["questions_collection"]
         
     def find_tag(self, selected_tag):
-        return self.my_collection.find({"tags":selected_tag},{"tags": 1, "_id": 0})
+        return self.__questions_collection.find({"tags":selected_tag},{"tags": 1, "_id": 0})
+
+    def find_all_documents(self):
+        return self.__questions_collection.find()
 
     def add_question(self, question):
         ''' Defines a _id for the question and verify if this id is unique. 
@@ -29,6 +58,4 @@ class QuestionDAO:
         question["_id"] = question["question_id"]
         key = {"_id":question["question_id"]}
         insert = {"$set":question}
-        self.my_collection.update_one(key, insert, upsert=True)
-
-
+        self.__questions_collection.update_one(key, insert, upsert=True)
